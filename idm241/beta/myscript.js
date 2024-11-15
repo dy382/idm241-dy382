434df3d8f7c46870737d7c25efc8914384a9bff7
dy382 <dy382@drexel.edu>
2024-11-15 12:04:48 -0500

    popup is flexed

--- idm241/beta/myscript.js
+++ idm241/beta/myscript.js
@@ -14,10 +14,13 @@ document.getElementById('saveButton').addEventListener('click', function() {
 document.getElementById('profileButton').addEventListener('click', function() {
     this.classList.toggle('clicked');
     let profilePopup = document.getElementById('profilePopup');
+    let profileBackground = document.getElementById('profileBackground');
 
     if (this.classList.contains('clicked')) {
         profilePopup.classList.remove('hidden');
+        profileBackground.classList.add('hidden');
     } else {
         profilePopup.classList.add('hidden');
+        profileBackground.classList.remove('hidden');
     }
 });
